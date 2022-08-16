e4b40260a937690a8dd1633b580af506a8cd3e05
Shyam Mahajan <shyammahajan2512@gmail.com>
2022-08-17 00:59:23 +0530

    finishing touches

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,12 +2,19 @@ import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data"
 import EventList from "../../components/events/event-list"
 import ResultsTitle from "../../components/events/results-title"
+import Button from "../../components/ui/button";
+
 
 const FilteredEventsPage = () => {
     const router = useRouter()
     const filterData = router.query.slug
     if(!filterData){
-        return <p className="center">Loading...</p>
+        return <>
+             <p className="center">Loading...</p>
+            <div className="center">
+                <Button link="/events">Show All Events</Button>
+            </div>
+        </>
     }
     
     const [year, month] = filterData;
@@ -15,13 +22,21 @@ const FilteredEventsPage = () => {
     const nummonth = +month;
 
     if(isNaN(nummonth) || isNaN(numyear)){
-        return <p className="center">Invalid Filter, please adjust your filter</p>
+        return <>
+            <p className="center">Invalid Filter, please adjust your filter</p>
+            <div className="center">
+                <Button link="/events">Show All Events</Button>
+            </div>
+        </>
     }
 
     const events = getFilteredEvents({year : numyear, month: nummonth})
 
     if(!events || !events.length){
-        return <p className="center">No events for the chosen filter!</p>
+        return <><p>No events for the chosen filter!</p>
+        <div className="center">
+                <Button link="/events">Show All Events</Button>
+            </div></>
     }
 
     const date = new Date(numyear, nummonth - 1)
