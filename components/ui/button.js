c2db00df6997636dd5d3492703a1fb725fb55418
Shyam Mahajan <shyammahajan2512@gmail.com>
2022-08-16 19:00:32 +0530

    event search component

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,9 +2,14 @@ import Link from "next/link"
 import classes from "./button.module.css"
 
 const Button = ({link, children}) => {
-    return <Link href={link}>
-        <a className={classes.btn}>{children}</a>
-    </Link>
+
+    if(link){
+        return <Link href={link}>
+            <a className={classes.btn}>{children}</a>
+        </Link>
+    }
+    return <button className={classes.btn}>{children}</button>
+
 }
 
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,22 @@
 import {getAllEvents} from "../../dummy-data"
+import { useRouter } from "next/router"
 import EventList from "../../components/events/event-list"
+import EventSearch from "../../components/events/events-search"
 
 const AllEventsPage = () => {
     const events = getAllEvents()
+    const router = useRouter()
+    
+    
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`
+        router.push(fullPath)
+    }
 
-    return <EventList items={events}></EventList>
+    return <>
+    <EventSearch onSearch={findEventsHandler}/>
+    <EventList items={events}></EventList>
+    </>
 }
 
 export default AllEventsPage;
