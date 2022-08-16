0b99a7262ecb57024fef31d8476d3b4091a0cb3c
Shyam Mahajan <shyammahajan2512@gmail.com>
2022-08-17 00:05:02 +0530

    filtered events with multiple dynamic query-params

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,34 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data"
+import EventList from "../../components/events/event-list"
+import ResultsTitle from "../../components/events/results-title"
+
 const FilteredEventsPage = () => {
-    return <div>
-        <h1>All Events FilteredEventsPage</h1>
-    </div>
+    const router = useRouter()
+    const filterData = router.query.slug
+    if(!filterData){
+        return <p className="center">Loading...</p>
+    }
+    
+    const [year, month] = filterData;
+    const numyear = +year;
+    const nummonth = +month;
+
+    if(isNaN(nummonth) || isNaN(numyear)){
+        return <p className="center">Invalid Filter, please adjust your filter</p>
+    }
+
+    const events = getFilteredEvents({year : numyear, month: nummonth})
+
+    if(!events || !events.length){
+        return <p className="center">No events for the chosen filter!</p>
+    }
+
+    const date = new Date(numyear, nummonth - 1)
+    return <>
+    <ResultsTitle date={date}/>
+    <EventList items={events}/>
+    </>
 }
 
 export default FilteredEventsPage; 
